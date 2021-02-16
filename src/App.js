c8a27fb99ab31a9d2b0f2373d5759f9923526eab
unktier <mappostorm@gmail.com>
2021-02-16 19:47:22 +0200

    find users current location

--- src/App.js
+++ src/App.js
@@ -1,6 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 const App = () => {
+    const [latitude, setLatitude] = useState(null);
+    const [longitude, setLongitude] = useState(null);
+
+    useEffect(() => {
+        window.navigator.geolocation.getCurrentPosition(
+            position => {
+                setLatitude(position.coords.latitude);
+                setLongitude(position.coords.longitude);                
+            },
+            err => {
+                console.log(err.message);
+            }
+        );
+    }, [latitude, longitude]);
+
     return (
         <div className="app">
             App
