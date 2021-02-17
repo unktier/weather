1567c866b2861e4c607a8916c7b758f9258fc32b
unktier <mappostorm@gmail.com>
2021-02-17 17:12:12 +0200

    make request to 7timer api

--- src/App.js
+++ src/App.js
@@ -2,24 +2,9 @@ import React, { useState, useEffect } from 'react';
 import DisplayWeather from './components/DisplayWeather';
 
 const App = () => {
-    const [latitude, setLatitude] = useState(null);
-    const [longitude, setLongitude] = useState(null);
-
-    useEffect(() => {
-        window.navigator.geolocation.getCurrentPosition(
-            position => {
-                setLatitude(position.coords.latitude);
-                setLongitude(position.coords.longitude);                
-            },
-            err => {
-                console.log(err.message);
-            }
-        );
-    }, [latitude, longitude]);
-
     return (
         <div className="app">
-            App
+            <DisplayWeather />
         </div>
     );
 };
--- src/components/DisplayWeather.js
+++ src/components/DisplayWeather.js
@@ -1,6 +1,37 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const DisplayWeather = () => {
+    const [latitude, setLatitude] = useState(null);
+    const [longitude, setLongitude] = useState(null);
+
+    useEffect(() => {
+        window.navigator.geolocation.getCurrentPosition(
+            position => {
+                setLatitude(position.coords.latitude);
+                setLongitude(position.coords.longitude);                
+            },
+            err => {
+                console.log(err.message);
+            }
+        );
+
+        const getLocation = async () => {
+            const response = await axios.get('http://www.7timer.info/bin/api.pl', {
+                params: {
+                    lon: longitude,
+                    lat: latitude,
+                    product: 'civil',
+                    output: 'json'
+                }
+            });
+            console.log(response);
+        };
+        getLocation();
+
+    }, [latitude, longitude]);
+
+
     return (
         <div className="display-weather">
             DisplayWeather
