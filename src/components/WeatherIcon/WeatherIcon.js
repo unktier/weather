96f7b80556ae91ba53974cd1fff6f6cdf0b0356a
unktier <mappostorm@gmail.com>
2021-02-28 14:59:39 +0200

    fix imports

--- src/components/WeatherIcon/WeatherIcon.js
+++ src/components/WeatherIcon/WeatherIcon.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ClearDay from './day.svg';
-import ClearNight from '/night.svg';
+import ClearNight from './night.svg';
 import PartlyCloudyDay from './cloudy-day-2.svg';
 import PartlyCloudyNight from './cloudy-night-2.svg';
 import CloudyNight from './cloudy-night-3.svg';
-import CloudyDay from '/cloudy-day-3.svg'
-import VeryCloudyDay from '/cloudy.svg';
+import CloudyDay from './cloudy-day-3.svg'
+import VeryCloudyDay from './cloudy.svg';
 import VeryCloudyNight from './cloudy.svg';
-import LightRain from './rain-4.svg';
+import LightRain from './rainy-4.svg';
 import IsolatedShower from './rainy-1.svg';
 import OccasionalShower from './rainy-3.svg';
 import LightSnow from './snowy-4.svg';
@@ -37,7 +37,7 @@ import Snow from './snowy-6.svg';
 const WeatherIcon = () => {
     return (
         <div className="weather-icon">
-            <img src={Day} alt="day" />
+            {/* <img src={Day} alt="day" /> */}
         </div>
     );
 };
