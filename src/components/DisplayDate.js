906ff4f6fd17d39958cb6682e38e8f2c86d0992f
unktier <mappostorm@gmail.com>
2021-03-02 21:42:56 +0200

    display current date

--- src/components/DisplayDate.js
+++ src/components/DisplayDate.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const DisplayDate = (props) => {
+    const [day, setDay] = useState(new Date().toDateString())
+
+
     return (
         <div className="display-date">
-            DisplayDate
+            {day}
         </div>
     );
 };
