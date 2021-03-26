20c9200fee0a08fa6aa2ecab1122bb9fdf4b511a
unktier <mappostorm@gmail.com>
2021-03-26 16:29:34 +0200

    destructure components from data object

--- src/components/CurrentLocation.js
+++ src/components/CurrentLocation.js
@@ -5,14 +5,14 @@ import axios from 'axios';
 const CurrentLocation = ({ long, lat }) => {
     useEffect(() => {
         const getLocation = async (latLong) => {
-            const data = await axios.get('https://api.opencagedata.com/geocode/v1', {
+            const { data: { results: { 0: { components } } } } = await axios.get('https://api.opencagedata.com/geocode/v1', {
                 params: {
                     key: KEY,
                     format: 'json',
                     q: latLong
                 }
             });
-            console.log(data);
+            console.log(components);
         };
         if (long && lat) {
             const paramData = `${lat.toFixed(6)},${long.toFixed(6)}`;
