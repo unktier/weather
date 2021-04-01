9135e31d235480da28c490e55d44ef8d1e1af00d
unktier <mappostorm@gmail.com>
2021-04-01 19:45:41 +0200

    remove console log

--- src/components/TimePoint.js
+++ src/components/TimePoint.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const TimePoint = ({ index, startTime }) => {
     const newTime = startTime + ((index + 1) * 3);
-    console.log(startTime);
+
     return (
         <div>
             {newTime}
