724f225c064adabb06f6e031b2396d428442b5c0
unktier <mappostorm@gmail.com>
2021-04-05 18:00:52 +0200

    set time for all cases

--- src/components/TimePoint.js
+++ src/components/TimePoint.js
@@ -1,8 +1,15 @@
 import React from 'react';
 
+const TIME_START = {
+    1: 3,
+    3: 9,
+    5: 15,
+    7: 21
+};
+
 const TimePoint = ({ index, startTime, timePoint, firstRender }) => {
     let newTime;
-    if (timePoint <= 15) {
+    if (timePoint <= TIME_START[firstRender]) {
         newTime = startTime + ((index + 1) * 3);
     } else {
         newTime = 2 + ((index) * 3);
