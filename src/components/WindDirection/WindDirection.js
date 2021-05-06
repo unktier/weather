9195300c01a2bf8cf05132f4d80e2b32769b6e49
unktier <mappostorm@gmail.com>
2021-05-06 18:23:54 +0200

    import current arrows

--- src/components/WindDirection/WindDirection.js
+++ src/components/WindDirection/WindDirection.js
@@ -1,5 +1,13 @@
 import React from 'react';
+import North from './Directions/north.png';
 import South from './Directions/south.png';
+import East from './Directions/east.png';
+import West from './Directions/west.png';
+import NorthEast from './Directions/northeast.png';
+import SouthEast from './Directions/southeast.png';
+import NorthWest from './Directions/northwest.png';
+import SouthWest from './Directions/southwest.png';
+
 
 const WindDirection = () => {
     return (
