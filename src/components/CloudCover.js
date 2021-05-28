6d82fc46a44e39ad7919fca972144e0b5561905e
unktier <mappostorm@gmail.com>
2021-05-28 15:34:28 +0200

    add cloud object

--- src/components/CloudCover.js
+++ src/components/CloudCover.js
@@ -1,5 +1,17 @@
 import React from 'react';
 
+const CLOUD_COVER = {
+    1: [0, 6],
+    2: [6, 19],
+    3: [19, 31],
+    4: [31, 44],
+    5: [44, 56],
+    6: [56, 69],
+    7: [69, 81],
+    8: [81, 94],
+    9: [94, 100]
+}
+
 const CloudCover = () => {
     return (
         <div className="cloud-cover">
