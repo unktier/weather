802c9d877e1c494b3be0842b35b5ec26be11e2a4
unktier <mappostorm@gmail.com>
2021-06-08 23:28:05 +0200

    update formatting

--- src/components/Wind.js
+++ src/components/Wind.js
@@ -29,7 +29,7 @@ const Wind = ({ posX, posY, isWeatherHover, wind10m, cloudCover, windIndex, wind
                 style={style}
             >
                 <CloudCover cloudCover={cloudCover} />
-                <div className="wind-speed">Wind:{wind10m.speed} m/s</div>
+                <div className="wind-speed">Wind:&emsp;{wind10m.speed} m/s</div>
                 <div className="wind-direction">{wind10m.direction}</div>
                 <WindDirection windDirection={wind10m.direction} />
             </div>
